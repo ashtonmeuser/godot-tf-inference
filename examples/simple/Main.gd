c73554e49789e2673e1e77bcfaac1ab42e1a9353
Ashton Meuser <ashtonmeuser@gmail.com>
2021-10-22 17:16:32 -0700

    Add GUI to example

--- examples/simple/Main.gd
+++ examples/simple/Main.gd
@@ -1,20 +1,49 @@
 extends Control
 
 const TFInference = preload("res://addons/godot-tf-inference/TFInference.gdns")
+var input = [0, 0] # Simple input X, Y to XOR model
+var output = [0] # Will hold result of model inference
+var inference_time := 0 # Time taken to infer XOR result
 
 func _ready():
-	var input = [1, 0] # Simple input X, Y to XOR model
-	var output # Will hold result of model inference
+	assert(!$LabelBit0.connect("pressed", self, "_toggle_bit", [0]))
+	assert(!$LabelBit1.connect("pressed", self, "_toggle_bit", [1]))
+	assert(!$LabelDescription/LinkButton.connect("pressed", self, "_open_repo"))
+	_update_labels()
+	_quick_example()
 
-	# New local instance of TFInference class
+func _update_labels():
+	$LabelBit0.text = String(input[0])
+	$LabelBit1.text = String(input[1])
+	$LabelBit2.text = String(int(round(output[0])))
+	$LabelSubtitle2.text = "Raw value: %1.4f" % output[0]
+	$LabelSubtitle3.text = "Inference time: %03dus" % inference_time
+	
+func _toggle_bit(i: int):
+	input[i] = (input[i] + 1) % 2
+	var t := OS.get_ticks_usec()
+	output = TfInferenceSingleton.infer(input)
+	inference_time = OS.get_ticks_usec() - t
+	_update_labels()
+
+func _open_repo():
+	var _err := OS.shell_open("https://github.com/ashtonmeuser/godot-tf-inference")
+
+func _quick_example():
+	# Here are a few ways to use TFInference class from GDScript
+	# Note that the addon is in a very early stage
+	# Please refer to the repo at github.com/ashtonmeuser/godot-tf-inference
+	# Feel free to open an issue or pull request
+
+	# Autoload TFInference singleton via Project Settings (recommended)
+	TfInferenceSingleton.load_model("./tf_xor_model")
+	TfInferenceSingleton.set_names("serving_default_dense_input:0", "StatefulPartitionedCall:0")
+	print("XOR(0, 0) = %s" % TfInferenceSingleton.infer([0, 0]))
+	
+	# Alternatively, a local instance of TFInference class
 	var tf = TFInference.new()
 	tf.load_model("./tf_xor_model")
 	tf.set_names("serving_default_dense_input:0", "StatefulPartitionedCall:0")
-	output = tf.infer(input)
-	print("Input: %s\tOutput: %s" % [input, output])
+	print("XOR(1, 0) = %s" % tf.infer([1, 0]))
 
-	# Alternatively, use a singleton
-	TfInferenceSingleton.load_model("./tf_xor_model")
-	TfInferenceSingleton.set_names("serving_default_dense_input:0", "StatefulPartitionedCall:0")
-	output = TfInferenceSingleton.infer(input)
-	print("Input: %s\tOutput: %s" % [input, output])
+# P.S. No, I did not want to draw the XOR gate
